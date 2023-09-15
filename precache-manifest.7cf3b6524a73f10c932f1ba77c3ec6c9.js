7069b21e58e5d99466027b34d8fe996ab397b95b
Dhiraj Kumar Varma Dommaraju <ddommaraju@csuchico.edu>
2023-09-15 15:19:44 -0700

    updated resume

---
+++ precache-manifest.7cf3b6524a73f10c932f1ba77c3ec6c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a315a99ffde72b8f91fb9a0d03403f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "90096dd1d830d1a9d658",
+    "url": "/portfolio/static/css/main.33c0eb41.chunk.css"
+  },
+  {
+    "revision": "64770be9759ac2eb893d",
+    "url": "/portfolio/static/js/2.75bd9dfa.chunk.js"
+  },
+  {
+    "revision": "90096dd1d830d1a9d658",
+    "url": "/portfolio/static/js/main.0f787701.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "77723fd630322da26f3279181b35ff41",
+    "url": "/portfolio/static/media/dhi2.77723fd6.jpg"
+  },
+  {
+    "revision": "ca065b6514ad03fd5cf589eee5baeaee",
+    "url": "/portfolio/static/media/dhiraj-dommaraju-resume.ca065b65.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "dhirajdkv.github.io/precache-manifest.250f6bd1fb55de93a9ad515a21cdaa55.js"
+  "/portfolio/precache-manifest.7cf3b6524a73f10c932f1ba77c3ec6c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("dhirajdkv.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
